648e7c6e850186efdd98f33fc01bcf0072bff5fc
Ricardo <rickatado@hotmail.co.uk>
2014-12-22 16:22:29 +0000

    This is the Eclipse version

---
+++ PinU1/src/com/pinultd/pinu1/signup.java
@@ -0,0 +1,13 @@
+package com.pinultd.pinu1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class signup extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sign_up);
+	}
+}
