9a4c6dbbb3c9726287cfcffcd9c70cb5fb7dc923
Ricardo <rickatado@hotmail.co.uk>
2014-12-27 13:51:05 +0000

    This is the Studio version

---
+++ PinU1/PinU1/src/main/java/com/pinultd/pinu1/fragment_main1.java
@@ -0,0 +1,32 @@
+package com.pinultd.pinu1;
+
+	import android.os.Bundle;
+	import android.support.v4.app.Fragment;
+	import android.view.LayoutInflater;
+	import android.view.View;
+	import android.view.ViewGroup;
+	import android.widget.TextView;
+
+	public class fragment_main1 extends Fragment {
+		public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
+
+		public static final My_fragment newInstance(String message)
+		{
+			My_fragment f = new My_fragment();
+			Bundle bdl = new Bundle(1);
+		    bdl.putString(EXTRA_MESSAGE, message);
+		    f.setArguments(bdl);
+		    return f;
+		}
+
+		@Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+	        Bundle savedInstanceState) {
+			String message = getArguments().getString(EXTRA_MESSAGE);
+			View v = inflater.inflate(R.layout.fragment_main1, container, false);
+			TextView messageTextView = (TextView)v.findViewById(R.id.textView);
+			messageTextView.setText(message);
+
+	        return v;
+	    }
+}
